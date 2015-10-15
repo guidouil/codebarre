3706719e0dda6c0abb768bb12d5f09706147f4cd
Guidouil <guidouil@gmail.com>
2015-10-15 15:28:58 +0200

    second commit

--- client/lib/functions/scan.js
+++ client/lib/functions/scan.js
@@ -2,31 +2,26 @@ scan = function () {
   cordova.plugins.barcodeScanner.scan(
     function (result) {
       if (result) {
-        searchEan(result);
+        var scanId = Barcodes.insert({
+          value: result.text,
+          format: result.format,
+          createdAt: new Date(),
+          product: {name:'', brands:''}
+        });
+        if (result.format.search('EAN') !== -1) {
+          Meteor.call('searchEan', result.text, scanId, function (error, result) {
+            if (error) {
+              alert('error' + error);
+            }
+            if (result) {
+              alert('result' + result);
+            }
+          });
+        }
       }
     },
     function (error) {
       alert('Scanning failed: ' + error);
     }
   );
-};
-
-var searchEan = function (scanned) {
-  if (scanned.format && scanned.format.search('EAN') !== -1) {
-    Meteor.call('searchEan', scanned, function (error, result) {
-      if (error) {
-        alert('error' + error);
-      }
-      if (result) {
-        alert('result' + result);
-      }
-    });
-  } else {
-    Barcodes.insert({
-      value: scanned.text,
-      format: scanned.format,
-      createdAt: new Date(),
-      product: {name:'not', brands:'EAN'}
-    });
-  }
 };
--- server/methods.js
+++ server/methods.js
@@ -1,21 +1,10 @@
 Meteor.methods({
-  searchEan: function (scanned) {
+  searchEan: function (codeEan, scanId) {
     var fact = HTTP.get('http://world.openfoodfacts.org/api/v0/product/' + scanned.text + '.json');
     if (fact && fact.status === 1) {
-      Barcodes.insert({
-        value: scanned.text,
-        format: scanned.format,
-        createdAt: new Date(),
-        product: fact.product
-      });
+      Barcodes.update({ _id: scanId }, {$set: { product: fact.product }});
       return true;
     } else {
-      Barcodes.insert({
-        value: scanned.text,
-        format: scanned.format,
-        createdAt: new Date(),
-        product: {name:'not', brands:'found'}
-      });
       return false;
     }
   }
