fcb682de5eacd6e5367065423980fe13af1da120
Guidouil <guidouil@gmail.com>
2015-10-19 22:43:34 +0200

    infinite scroll

--- client/templates/home/home_script.js
+++ client/templates/home/home_script.js
@@ -1,6 +1,6 @@
 Template.home.helpers({
   scans: function () {
-    return Scans.find({}, {sort: {createdAt: -1}, limit: 10}).fetch();
+    return Scans.find({}, {sort: {createdAt: -1}}).fetch();
   },
   fromNow: function (date) {
     if (date) {
@@ -20,7 +20,16 @@ Template.home.events({
   },
   'click [data-action=delete]': function (evt) {
     Scans.remove({_id:this._id});
-  }
+  },
+  'scroll .ui.list': function (event) {
+      event.preventDefault();
+      let el = event.target;
+      if (el.offsetHeight != el.scrollHeight) {
+        if (el.scrollTop + el.offsetHeight > el.scrollHeight - 200) {
+          handle.loadNextPage();
+        }
+      }
+    }
 });
 
 Template.home.onRendered(function ( ){
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,3 @@
-Meteor.publish('Scans', function () {
-  return Scans.find({}, { reactive: true, sort: {createdAt: -1}, limit: 10 });
+Meteor.publish('Scans', function (limit) {
+  return Scans.find({}, { reactive: true, sort: {createdAt: -1}, limit: limit });
 });
