284fca26bebb1dd5c3a24bd65fcfd65d555e896a
Guidouil <guidouil@gmail.com>
2015-10-21 00:36:48 +0200

    android

--- mobile-config.js
+++ mobile-config.js
@@ -17,10 +17,10 @@ App.icons({
   'ipad_2x': 'resources/icons/icon-152x152.png',
 
   // Android
-  // 'android_ldpi': 'resources/icons/icon-36x36.png',
-  // 'android_mdpi': 'resources/icons/icon-48x48.png',
-  // 'android_hdpi': 'resources/icons/icon-72x72.png',
-  // 'android_xhdpi': 'resources/icons/icon-96x96.png'
+  'android_ldpi': 'resources/icons/icon-36x36.png',
+  'android_mdpi': 'resources/icons/icon-48x48.png',
+  'android_hdpi': 'resources/icons/icon-72x72.png',
+  'android_xhdpi': 'resources/icons/icon-96x96.png'
 });
 
 App.launchScreens({
@@ -36,18 +36,18 @@ App.launchScreens({
   'ipad_landscape_2x': 'resources/splash/Default-Landscape@2x.png', // 2048x1536
 
   // Android
-  // 'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  // 'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  // 'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  // 'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  // 'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  // 'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  // 'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  // 'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
-App.setPreference('StatusBarBackgroundColor', '#FFFFFF');
+App.setPreference('StatusBarBackgroundColor', '#000000');
 App.setPreference('Orientation', 'default');
 App.setPreference('Orientation', 'all', 'ios');
 
