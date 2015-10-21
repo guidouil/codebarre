4fbfca9d8be4940249d8b1d2284a63a6a282552b
Guidouil <guidouil@gmail.com>
2015-10-21 23:19:25 +0200

    locale product finaly working

--- server/methods.js
+++ server/methods.js
@@ -4,27 +4,32 @@ Meteor.methods({
     var product = Products.findOne({_id: codeScanned});
     if (product) {
       return updScanWithProduct(scanId, product);
-    } else if (codeFormat.search('EAN') !== -1) {
-      // Search from openfoodfacts.org
-      var response = HTTP.get('http://world.openfoodfacts.org/api/v0/product/' + codeScanned + '.json');
-      if (response && response.statusCode === 200 && response.data && response.data.status === 1) {
-        Products.insert(response.data.product);
-        return updScanWithProduct(scanId, response.data.product);
-      } else {
-        var productNotFound = {
-          _id: codeScanned,
-          product_name: 'notFound',
-          brands: '404'
-        };
-        return updScanWithProduct(scanId, productNotFound);
-      }
     } else {
-      // insert local product
-      Products.insert({
-        _id: codeScanned,
-        product_name: codeScanned,
-        brands: codeFormat
-      });
+      if (codeFormat.search('EAN') !== -1) {
+        // Search from openfoodfacts.org
+        var response = HTTP.get('http://world.openfoodfacts.org/api/v0/product/' + codeScanned + '.json');
+        if (response && response.statusCode === 200 && response.data && response.data.status === 1) {
+          Products.insert(response.data.product);
+          return updScanWithProduct(scanId, response.data.product);
+        } else {
+          var productNotFound = {
+            _id: codeScanned,
+            product_name: 'notFound',
+            brands: '404'
+          };
+          Products.insert(productNotFound);
+          return updScanWithProduct(scanId, productNotFound);
+        }
+      } else {
+        // insert local product
+        var productNotEan = {
+          _id: codeScanned,
+          product_name: codeScanned,
+          brands: codeFormat
+        };
+        Products.insert(productNotEan);
+        return updScanWithProduct(scanId, productNotEan);
+      }
     }
   }
 });
