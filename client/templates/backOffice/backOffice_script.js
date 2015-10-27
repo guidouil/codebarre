4d4e47ce9699f0bbde635c2869ff369687f88e2b
Guidouil <guidouil@gmail.com>
2015-10-27 15:51:16 +0100

    B.O. templates

---
+++ client/templates/backOffice/backOffice_script.js
@@ -0,0 +1,13 @@
+Template.backOffice.helpers({
+});
+
+Template.backOffice.events({
+});
+
+Template.backOffice.onRendered(function () {
+
+});
+
+Template.backOffice.onDestroyed(function () {
+
+});
